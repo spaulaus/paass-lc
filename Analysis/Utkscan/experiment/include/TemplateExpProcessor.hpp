5586156f173702d9ca7859ca1e5efac13088085c
S.V. Paulauskas <stanpaulauskas@gmail.com>
2018-03-11 15:05:09 -0400

    Updating Experiment template to make better use of RootHandler

--- Analysis/Utkscan/experiment/include/TemplateExpProcessor.hpp
+++ Analysis/Utkscan/experiment/include/TemplateExpProcessor.hpp
@@ -1,61 +1,43 @@
-/** \file TemplateExpProcessor.hpp
- * \brief Example class for experiment specific setups
- *\author S. V. Paulauskas
- *\date May 20, 2016
- */
+///@file TemplateExpProcessor.hpp
+///@brief Example class for experiment specific setups
+///@author S. V. Paulauskas
+///@date May 20, 2016
 #ifndef __TEMPLATEEXPPROCESSOR_HPP_
 #define __TEMPLATEEXPPROCESSOR_HPP_
-
-#include <fstream>
-
 #include "EventProcessor.hpp"
 
-#include <TFile.h>
-#include <TTree.h>
-#include <TH2D.h>
-#include <TH1D.h>
-
-/// Working template class for experiment processors
+/// A processor that provides a working, concrete example for users to follow when creating a customized experiment
+/// processor.
 class TemplateExpProcessor : public EventProcessor {
 public:
-    /** Default Constructor */
+    ///Default Constructor
     TemplateExpProcessor();
 
-    /** Constructor to take an argument for the gamma cutoff */
+    ///Constructor to take an argument for the gamma cutoff. You can define anything as arguments here. You'll just
+    /// add them to DetectoDriverXmlParser::ParseProcessors when its newing the class.
     TemplateExpProcessor(const double &gcut);
 
-    /** Default Destructor */
+    ///Default Destructor that doesn't need to do anything. Anything that you new in this processor should be cleaned
+    /// up here.
     ~TemplateExpProcessor();
 
-    /** Declare the plots used in the analysis */
+    /// Declare the plots used in the analysis
     void DeclarePlots(void);
 
-    /** PreProcess does nothing since this is solely dependent on results
-     from other Processors*/
-    bool PreProcess(RawEvent &event);
-
-    /** Process the event
-    * \param [in] event : the event to process
-    * \return Returns true if the processing was successful */
+    /// Process the events that depend on other unassociated detector types.
+    ///@param [in] event : the event to process
+    ///@returns true if the processing was successful
     bool Process(RawEvent &event);
 
 private:
-    /** Sets the detectors that are associated with this processor */
+    ///Sets the detector types that are associated with this processor
     void SetAssociatedTypes(void);
 
-    /** Sets up the ASCII output file stream */
-    void SetupAsciiOutput(void);
-
-    std::ofstream *poutstream_; //!< Pointer to ouptut ASCII file stream.
-    double gCutoff_; //!< Variable used to set gamma cutoff energy
-
-    /** Method to setup the ROOT output, tree and histograms */
+    /// Method to register our tree and branches when constructing the class
     void SetupRootOutput(void);
 
-    TFile *prootfile_; //! pointer to root file
-    TTree *proottree_; //! pointer to root tree
-    TH2D *ptvsge_;    //! pointer to ROOT 2D histogram
-    TH1D *ptsize_;     //! pointer to ROOT 1D histogram
+    double gCutoff_; //!< Variable used to set gamma cutoff energy
+    TTree *tree_; //!< Pointer to the tree that we're going to define in the constructor
 };
 
 #endif
