f947d25f6d2f4d1525b24271112d16e8877c2d8b
S.V. Paulauskas <stanpaulauskas@gmail.com>
2018-05-13 16:47:24 -0400

    Fixing bugs in TraditionalCfd
    1) We checked the size was 0 instead of empty. Empty is a better way to do this.
    2) We added cfd_.clear() so that we can calculate the phase in a loop where the object isn't
    destroyed.
    3) The int cast wasn't necessary.

--- Analysis/Resources/source/TraditionalCfd.cpp
+++ Analysis/Resources/source/TraditionalCfd.cpp
@@ -11,20 +11,21 @@ using namespace std;
 double TraditionalCfd::CalculatePhase(const std::vector<double> &data, const std::pair<double, double> &pars,
                                       const std::pair<unsigned int, double> &max,
                                       const std::pair<double, double> baseline) {
-    if (data.size() == 0)
-        throw range_error("PolynomialCfd::CalculatePhase - The data vector was empty!");
+    if (data.empty())
+        throw range_error("TraditionalCfd::CalculatePhase - The data vector was empty!");
 
     double fraction = pars.first;
     auto delay = (unsigned int) pars.second;
 
+    cfd_.clear();
     for (unsigned int i = 0; i < data.size() - delay; i++)
         cfd_.push_back(fraction * (data[i] - data[i + delay]));
 
     pair<double, double> xyBelowZero(0, 0);
     pair<double, double> xyAboveZero(0, 0);
 
-    auto cfdMinPosition = int(min_element(cfd_.begin(), cfd_.end()) - cfd_.begin());
-    auto cfdMaxPosition = int(max_element(cfd_.begin(), cfd_.end()) - cfd_.begin());
+    auto cfdMinPosition = min_element(cfd_.begin(), cfd_.end()) - cfd_.begin();
+    auto cfdMaxPosition = max_element(cfd_.begin(), cfd_.end()) - cfd_.begin();
 
     for (int i = cfdMinPosition; i < cfdMaxPosition; i++) {
         if(cfd_.at(i) > 0) {
