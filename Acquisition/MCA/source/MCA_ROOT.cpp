03f35789088324b78bf89b9f75ba5c84bbd41aba
S.V. Paulauskas <stanpaulauskas@gmail.com>
2018-09-11 07:03:12 -0400

    Documentation and formatting update

--- Acquisition/MCA/source/MCA_ROOT.cpp
+++ Acquisition/MCA/source/MCA_ROOT.cpp
@@ -1,4 +1,7 @@
-///@authors K. Smith, C. R. Thornsberry
+/// @file MCA_ROOT.cpp
+/// @brief Class derived from MCA that writes the MCA spectra to a root file.
+/// @author K. Smith, C. R. Thornsberry, S. V. Paulauskas
+/// @date September 11, 2018
 #include "MCA_ROOT.h"
 #include "AcquisitionInterface.hpp"
 #include "Display.h"
@@ -19,21 +22,22 @@ MCA_ROOT::~MCA_ROOT() {
 }
 
 bool MCA_ROOT::OpenFile(const char *basename) {
-
-    std::string message =
-            std::string("Creating new empty ROOT histogram ") + std::string(basename) +
-            std::string(".root");
+    std::string message = std::string("Creating new empty ROOT histogram ") + std::string(basename) + std::string(".root");
     Display::LeaderPrint(message);
+
     //We redirect stderr, to catch ROOT error messages
     cerr_redirect *redirect = new cerr_redirect("Pixie16msg.txt");
+
     //Try and open the file
     _file = new TFile(Form("%s.root", basename), "RECREATE");
     isOpen_ = _file->IsOpen() && _file->IsWritable();
+
     if (Display::StatusPrint(!isOpen_)) {
         //Print out the ROOT error message
         redirect->Print();
         return isOpen_;
     }
+
     redirect->Print();
     delete redirect;
 
@@ -41,9 +45,7 @@ bool MCA_ROOT::OpenFile(const char *basename) {
     for (int card = 0; card < _pif->GetNumberOfModules(); card++) {
         for (unsigned int ch = 0; ch < _pif->GetNumberOfChannels(); ch++) {
             int id = (card + 1) * 100 + ch;
-            _histograms[id] = new TH1F(Form("h%d%02d", card, ch),
-                                       Form("Mod %d Ch %d", card, ch), ADC_SIZE,
-                                       0, ADC_SIZE);
+            _histograms[id] = new TH1F(Form("h%d%02d", card, ch), Form("Mod %d Ch %d", card, ch), ADC_SIZE, 0, ADC_SIZE);
         }
     }
     _file->Write(0, TObject::kWriteDelete);
@@ -55,7 +57,9 @@ bool MCA_ROOT::OpenFile(const char *basename) {
 TH1F *MCA_ROOT::GetHistogram(int mod, int ch) {
     int id = (mod + 1) * 100 + ch;
     auto loc = _histograms.find(id);
+
     TH1F *histogram = nullptr;
+
     if (loc != _histograms.end())
         histogram = loc->second;
 
